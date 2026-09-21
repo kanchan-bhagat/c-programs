5fc1a9af09e6e2c3c4a34748e2f42169b03db44b
kanchan-bhagat <kanchanbhagat1498@gmail.com>
2026-09-21 11:11:44 +0530

    Add files via upload
    Added Fibonacci Program

---
+++ Fibonancci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int n, a = 0, b = 1, c, i;
+
+    clrscr();
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for(i = 1; i <= n; i++)
+    {
+        printf("%d ", a);
+
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    getch();
+    return 0;
+}
